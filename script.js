cc704b709644776e29baf13fb3b52b82c4c151b1
Will <willbickham3@gmail.com>
2022-07-24 06:31:16 -0400

    Initialize variables

---
+++ script.js
@@ -0,0 +1,7 @@
+const previousOperand = document.querySelector('.previous-operand');
+const currentOperand = document.querySelector('.current-operand')
+const operand = document.querySelectorAll('.operand');
+const operator = document.querySelectorAll('.operator');
+const clearBtn = document.querySelector('.clear');
+const deleteBtn = document.querySelector('.delete');
+
