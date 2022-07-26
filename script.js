a2d41897825db65e8a9c685968778a27cfc009da
Will <willbickham3@gmail.com>
2022-07-26 03:28:14 -0400

    Name functions for calculator operations

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ const operator = document.querySelectorAll('.operator');
 const clearBtn = document.querySelector('.clear');
 const deleteBtn = document.querySelector('.delete');
 
+function add(a, b) {
+    result = a + b;
+    console.log(result)}
+
+function subtract(a, b) {
+    result = a - b;
+    console.log(result)}
+
+function multiply(a, b) {
+    result = a * b;
+    console.log(result)}
+
+function divide(a, b) {
+    result = a / b;
+    console.log(result)
+}
+
+function operate(operator, a, b) {}
