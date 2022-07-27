3f04053ebffaabd30ea56156edb51d45015a1244
Will <willbickham3@gmail.com>
2022-07-26 23:32:38 -0400

    Add operate function to perform basic arithmetic give 3 parameters

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const operator = document.querySelectorAll('.operator');
 const clearBtn = document.querySelector('.clear');
 const deleteBtn = document.querySelector('.delete');
 
+let symbol = "*";
+let a = 3;
+let b = 3;
+
 function add(a, b) {
     result = a + b;
     console.log(result)}
@@ -22,4 +26,23 @@ function divide(a, b) {
     console.log(result)
 }
 
-function operate(operator, a, b) {}
+function operate(symbol, a, b) {
+    if (symbol == "+") {
+        add(a, b);
+        return
+    }
+    else if (symbol == "-") {
+        subtract(a, b)
+        return
+    }
+    else if (symbol == "*") {
+        multiply(a, b)
+        return
+}   else if (symbol == "÷") {
+        divide(a, b)
+        return
+}
+    else {
+        return console.log("error");
+    }
+}
